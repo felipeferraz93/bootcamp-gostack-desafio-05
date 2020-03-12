56e8bfff42a00632297e1b1bccbe7a08860dc43a
Felipe <felipe.ferraz.eng@gmail.com>
2020-03-11 21:08:32 -0300

    feature/erros e pagincao

--- src/Pages/Main/index.js
+++ src/Pages/Main/index.js
@@ -8,7 +8,7 @@ import api from '../../services/api';
 
 import Container from '../../components/Container';
 
-import { Form, SubmitButton, List} from './styles';
+import { Form, SubmitButton, List, ErrorMessage} from './styles';
 
 
 
@@ -17,6 +17,8 @@ export default class Main extends Component {
     newRepo: '',
     repositories: [],
     loading: false,
+    error:null,
+    errormsg: '',
   };
 
   componentDidMount(){
@@ -40,12 +42,30 @@ export default class Main extends Component {
     this.setState({ newRepo: e.target.value });
   };
 
+
+
   handleSubmit = async e => {
     e.preventDefault();
 
-    this.setState({ loading: true });
+    this.setState({ loading: true, error:false });
 
-    const { newRepo, repositories } = this.state;
+    try{
+
+    /**
+     * verificação se o input esta vazio
+     */
+
+    const { newRepo, repositories} = this.state;
+
+    if (newRepo ==='') throw new SyntaxError('Digite algum respositório!')
+
+    /**
+     * verificação se já existe o repositorio ja salvo
+     */
+    const hasRepo = repositories.find(r => r.name === newRepo);
+
+    if (hasRepo) throw new SyntaxError ('Repositório duplicado');
+
 
     const response = await api.get(`/repos/${newRepo}`);
 
@@ -57,11 +77,22 @@ export default class Main extends Component {
         repositories: [...repositories, data ],
         newRepo: '',
         loading: false,
+
       })
+
+    }catch (error){
+      this.setState({ error: true,errormsg: error.message});
+
+    }
+    finally{
+      this.setState({ loading: false });
+    }
+
+
   };
 
   render() {
-    const { newRepo, repositories, loading } = this.state;
+    const { newRepo, repositories, loading, error,errormsg} = this.state;
 
     return (
       <Container>
@@ -70,12 +101,13 @@ export default class Main extends Component {
           Repositórios
         </h1>
 
-        <Form onSubmit={this.handleSubmit}>
+        <Form onSubmit={this.handleSubmit} error={error}>
           <input
             type="text"
             placeholder="Adicionar respositório"
             value={newRepo}
             onChange={this.handleInputChange}
+
           />
 
           <SubmitButton loading={loading}>
@@ -86,7 +118,17 @@ export default class Main extends Component {
             ( <FaPlus color="#fff" size={14} />)}
 
           </SubmitButton>
+
+
+
         </Form>
+
+        {error &&
+        <ErrorMessage error={errormsg}>
+            <p>{errormsg}</p>
+        </ErrorMessage>
+        }
+
 
         <List>
               {repositories.map(repository =>(
